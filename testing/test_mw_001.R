d1ad49fd5c4f130e39b478c071991f2825ed67c0
Bram W Stone <stone.bram@gmail.com>
2020-02-05 16:00:10 -0700

    Added testing documents to compare qSIP package output to manual calculations

---
+++ testing/test_mw_001.R
@@ -0,0 +1,84 @@
+# Testing of the calculation of MW values between qSIP package and manual
+# calculation code: 000
+
+############################################
+# qsip package
+############################################
+
+mdq <- specify_qsip(md,
+                    abund='qPCR.16S.copies.ul',
+                    density='density.g.ml',
+                    rep_id='RepID',
+                    iso='18O',
+                    iso_trt='tmt')
+
+# calculate WADs
+mdq <- calc_mw(mdq, separate_label=F, separate_light=T)
+
+# convert to data frame, use c() to remove attributes
+mw <- data.frame(OTU=names(mdq@qsip[['mw_label']]),
+                 mw_label=c(mdq@qsip[['mw_label']]),
+                 mw_light=c(mdq@qsip[['mw_light']]),
+                 stringsAsFactors=F)
+
+mw <- reshape(mw,
+              idvar='OTU',
+              varying=list(c('mw_label', 'mw_light')),
+              times=c('18O', '16O'),
+              timevar='tmt',
+              v.names='mw',
+              direction='long')
+rownames(mw) <- NULL
+
+mw_qsip <- mw
+
+
+
+############################################
+# manual calculation
+############################################
+
+# add treatment (isotope) data for grouping
+mw <- merge(wads_man, unique(mdl[,c('RepID', 'tmt')]), all.x=T)
+  
+# split by treatment group and taxon
+mw <- split(mw, interaction(mw$tmt, mw$OTU))
+
+# average WADs, remove replicate data
+mw <- lapply(mw, function(x) {
+  x$wad <-  mean(x$wad, na.rm=T)
+  x$RepID <- NULL
+  x[1,]
+})
+
+# separate light and heavy MWs
+mw_l <- mw[grep('16O', names(mw))]
+mw_h <- mw[grep('18O', names(mw))]
+
+# calculate MW-light
+mw_l <- lapply(mw_l, function(x) {
+  gc <- (1 / 0.083506) * (x$wad - 1.646057)
+  x$mw <- (0.496 * gc) + 307.691
+  x
+})
+
+# calculate MW-heavy
+mw_h <- Map(function(x,y) {
+  x$mw <- (((x$wad - y$wad)/y$wad) + 1) * y$mw
+  x
+}, mw_h, mw_l)
+
+# combine
+mw <- rbind(do.call(rbind, mw_l),
+            do.call(rbind, mw_h))
+rownames(mw) <- NULL
+mw_manual <- mw
+
+
+############################################
+# compare calculations
+############################################
+
+mw_000 <- merge(mw_qsip, mw_manual[,c('OTU', 'tmt', 'mw')], 
+                by=c('OTU', 'tmt'), 
+                suffixes=c('_qsip', '_manual'))
---
+++ testing/test_wads.R
@@ -0,0 +1,70 @@
+# Testing of the calculation of WAD values between qSIP package and manual
+
+
+############################################
+# qsip package
+############################################
+
+mdq <- specify_qsip(md,
+                    abund='qPCR.16S.copies.ul',
+                    density='density.g.ml',
+                    rep_id='RepID',
+                    iso='18O',
+                    iso_trt='tmt')
+
+# calculate WADs
+mdq <- calc_wad(mdq)
+
+# convert to data frame
+wads <- mat_to_df(mdq@qsip[['wad']], 'wad')
+wads_qsip <- wads[!is.na(wads$wad),]
+
+
+############################################
+# manual calculation
+############################################
+
+# split by sample/replicate and taxon
+wads <- split(mdl, interaction(mdl$RepID, mdl$OTU))
+
+# If any sample has NA for density, make those qPCR abundances NA as well
+# calculate relative abundance of each taxa in each fraction
+wads <- lapply(wads, function(x) {
+  x$rel_abund <- x$Abundance * x$qPCR.16S.copies.ul
+  x$rel_abund <- x$rel_abund / sum(x$rel_abund, na.rm=T)
+  x
+})
+
+# multiply relative abundances by density, then sum
+wads <- lapply(wads, function(x) {
+  x$wad <- x$density.g.ml * x$rel_abund
+  x$wad <- sum(x$wad, na.rm=T)
+  x
+})
+
+# return single row, remove fraction info
+wads <- lapply(wads, function(x) {
+  x <- x[1,]
+  x[,!names(x) %in% grep('fraction|Sample', names(x), value=T)]
+})
+
+# re-combine
+wads <- do.call(rbind, wads)
+
+# remove NA wads (where wad=0)
+wads <- wads[wads$wad > 0,]
+wads <- wads[,c('RepID', 'OTU', 'wad')]
+rownames(wads) <- NULL
+wads_man <- wads
+
+
+############################################
+# compare calculations
+############################################
+
+wads_qsip <- wads_qsip[order(wads_qsip$OTU, wads_qsip$RepID),]
+wads_man <- wads_man[order(wads_man$OTU, wads_man$RepID),]
+
+wads <- merge(wads_qsip, wads_man,
+              by=c('RepID', 'OTU'),
+              suffixes=c('_qsip', '_manual'))
