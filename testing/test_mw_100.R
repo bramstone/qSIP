2331f0a59ba1ff35fd5ac2ced482ade1800593d1
Bram W Stone <stone.bram@gmail.com>
2020-02-07 10:27:35 -0700

    Added calc code 100 to MW testing

--- testing/test_mw_100.R
+++ testing/test_mw_100.R
@@ -16,20 +16,22 @@ mdq <- specify_qsip(md,
 # calculate WADs
 mdq <- calc_mw(mdq, separate_label=F, separate_light=F)
 
-# convert to data frame, use c() to remove attributes
-mw <- data.frame(OTU=names(mdq@qsip[['mw_label']]),
-                 mw_label=c(mdq@qsip[['mw_label']]),
-                 mw_light=c(mdq@qsip[['mw_light']]),
-                 stringsAsFactors=F)
+mwh <- mat_to_df(mdq@qsip[['mw_label']], 'mw_label')
+mwl <- mat_to_df(mdq@qsip[['mw_light']], 'mw_light')
+
+mw <- merge(mwh, mwl, by=c('RepID', 'OTU'), all.x=T)
 
 mw <- reshape(mw,
-              idvar='OTU',
+              idvar=c('RepID', 'OTU'),
               varying=list(c('mw_label', 'mw_light')),
               times=c('18O', '16O'),
               timevar='tmt',
               v.names='mw',
               direction='long')
 rownames(mw) <- NULL
+
+names(mw)[1] <- 'group'
+mw$group <- sub('(.*)\\.(.*)', '\\2', mw$group)
 
 mw_qsip <- mw
 
@@ -39,11 +41,11 @@ mw_qsip <- mw
 # manual calculation
 ############################################
 
-# add treatment (isotope) data for grouping
-mw <- merge(wads_man, unique(mdl[,c('RepID', 'tmt')]), all.x=T)
+# add treatment (isotope) and group data for grouping
+mw <- merge(wads_man, unique(mdl[,c('RepID', 'tmt', 'group')]), all.x=T)
 
-# split by treatment group and taxon
-mw <- split(mw, interaction(mw$tmt, mw$OTU))
+# split by treatment, group, and taxon
+mw <- split(mw, interaction(mw$tmt, mw$group, mw$OTU))
 
 # average WADs, remove replicate data
 mw <- lapply(mw, function(x) {
@@ -51,6 +53,10 @@ mw <- lapply(mw, function(x) {
   x$RepID <- NULL
   x[1,]
 })
+
+# recombine, then split by group and treatment
+mw <- do.call(rbind, mw)
+mw <- split(mw, interaction(mw$tmt, mw$group))
 
 # separate light and heavy MWs
 mwl <- mw[grep('16O', names(mw))]
@@ -60,10 +66,11 @@ mwh <- mw[grep('18O', names(mw))]
 mwl <- lapply(mwl, function(x) {
   gc <- (1 / 0.083506) * (x$wad - 1.646057)
   x$mw <- (0.496 * gc) + 307.691
+  gc <- NULL
   x
 })
 
-# calculate MW-heavy
+# calculate MW-heavy across groups
 mwh <- Map(function(x,y) {
   x$mw <- (((x$wad - y$wad)/y$wad) + 1) * y$mw
   x
@@ -80,6 +87,6 @@ mw_manual <- mw
 # compare calculations
 ############################################
 
-mw_100 <- merge(mw_qsip, mw_manual[,c('OTU', 'tmt', 'mw')],
-                by=c('OTU', 'tmt'),
+mw_100 <- merge(mw_qsip, mw_manual[,!names(mw_manual) %in% 'wad'],
+                by=c('OTU', 'group', 'tmt'),
                 suffixes=c('_qsip', '_manual'))
