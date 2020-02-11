a38ed7a4a7d81ddc5c03d8b63de96fb6c5709b57
Bram W Stone <stone.bram@gmail.com>
2020-02-11 11:11:33 -0700

    Fixed merge issue in collation of MW-qsip

--- testing/test_mw_011.R
+++ testing/test_mw_011.R
@@ -6,20 +6,20 @@
 ############################################
 
 # first need to modify replicate number column so that they are unique across all groups
-rep_matching <- unique(md@sam_data[,c('RepID', 'tmt')])
-rep_matching <- as(rep_matching, 'data.frame')
-rep_matching <- split(rep_matching, rep_matching$tmt)
-# sapply(rep_matching, nrow) # check row number - 12 in both 16O and 18O - it matches
+rep_matching_011 <- unique(md@sam_data[,c('RepID', 'tmt')])
+rep_matching_011 <- as(rep_matching_011, 'data.frame')
+rep_matching_011 <- split(rep_matching_011, rep_matching_011$tmt)
+# sapply(rep_matching_011, nrow) # check row number - 12 in both 16O and 18O - it matches
 # add in numbers - NOTE: sample names are aligned alphabetically such that the numbers in each treatment correspond
-rep_matching <- lapply(rep_matching, function(x) {x$rep_number <- 1:nrow(x); x})
-rep_matching <- do.call(rbind, rep_matching)
+rep_matching_011 <- lapply(rep_matching_011, function(x) {x$rep_number <- 1:nrow(x); x})
+rep_matching_011 <- do.call(rbind, rep_matching_011)
 # get into same format as md@sam_data
-rep_matching <- merge(as(md@sam_data, 'data.frame'), rep_matching, all.x=T)
-rep_matching <- rep_matching[match(md@sam_data$sample.rep.fraction, rep_matching$sample.rep.fraction),]
+rep_matching_011 <- merge(as(md@sam_data, 'data.frame'), rep_matching_011, all.x=T)
+rep_matching_011 <- rep_matching_011[match(md@sam_data$sample.rep.fraction, rep_matching_011$sample.rep.fraction),]
 
 # combine new replicate numbers with data
 mdq <- md
-mdq@sam_data$rep_number <- rep_matching$rep_number
+mdq@sam_data$rep_number <- rep_matching_011$rep_number
 
 mdq <- specify_qsip(mdq,
                     abund='qPCR.16S.copies.ul',
@@ -32,20 +32,14 @@ mdq <- specify_qsip(mdq,
 # calculate WADs
 mdq <- calc_mw(mdq, separate_label=T, separate_light=T)
 
-mwh <- mat_to_df(mdq@qsip[['mw_label']], 'mw_label')
-mwl <- mat_to_df(mdq@qsip[['mw_light']], 'mw_light')
+mwh <- mat_to_df(mdq@qsip[['mw_label']], 'mw')
+mwl <- mat_to_df(mdq@qsip[['mw_light']], 'mw')
+
+mwh$tmt <- '18O'
+mwl$tmt <- '16O'
 
 # convert to data frame, use c() to remove attributes
-mw <- merge(mwh, mwl, by=c('RepID', 'OTU'), all.x=T)
-
-mw <- reshape(mw,
-              idvar=c('RepID', 'OTU'),
-              varying=list(c('mw_label', 'mw_light')),
-              times=c('18O', '16O'),
-              timevar='tmt',
-              v.names='mw',
-              direction='long')
-rownames(mw) <- NULL
+mw <- rbind(mwh, mwl)
 
 mw_qsip <- mw
 
@@ -55,7 +49,7 @@ mw_qsip <- mw
 ############################################
 
 # add treatment (isotope) data and replicate numbers for grouping
-mw <- merge(wads_man, unique(rep_matching[,c('RepID', 'tmt', 'rep_number')]), all.x=T)
+mw <- merge(wads_man, unique(rep_matching_011[,c('RepID', 'tmt', 'rep_number')]), all.x=T)
 
 # separate light and heavy WADs
 mwh <- mw[mw$tmt=='18O',]
@@ -88,7 +82,7 @@ mw <- reshape(mw,
 rownames(mw) <- NULL
 
 # get RepID again
-mw <- merge(unique(rep_matching[,c('RepID', 'tmt', 'rep_number')]),
+mw <- merge(unique(rep_matching_011[,c('RepID', 'tmt', 'rep_number')]),
             mw,
             all.y=T)
 mw$rep_number <- NULL
